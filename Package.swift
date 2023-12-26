aa52007ef008a76b314a7ad8f30cd0fc582c47fd
Raul Costa Junior <raulcostajunior@gmail.com>
2023-12-26 09:36:45 +0100

    Add platform information to the package.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "sudokukit",
+    platforms: [
+        .macOS(.v10_13),  // Also supports Linux and Windows
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
